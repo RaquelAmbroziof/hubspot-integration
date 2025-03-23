04c718bbd95e47b014483be3f73b56bf6b54e85b
Raquel <ambrozioraquel@gmail.com>
2025-03-23 00:31:53 -0300

    inicializar projeto com Java 17, Spring Boot 3.4.4 e Apache Maven 3.9.9

---
+++ src/test/java/com/example/hubspot_integration/HubspotIntegrationApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.hubspot_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubspotIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Verifica se o contexto Spring carrega com sucesso.
+
+	}
+
+}
